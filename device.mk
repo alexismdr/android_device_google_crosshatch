010a79dac27249746d81c77e6d74b3e3136a7c3a
Alexis Mercier des Rochettes <apernouille@gmail.com>
2024-09-02 00:58:30 +0200

    sensors : update HAL

--- device.mk
+++ device.mk
@@ -476,7 +476,8 @@ PRODUCT_PACKAGES += \
 ##    libgoogle_camera_hal_tests
 
 PRODUCT_PACKAGES += \
-    sensors.$(PRODUCT_HARDWARE)
+    sensors.$(PRODUCT_HARDWARE) \
+    android.hardware.sensors-service.multihal
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:vendor/etc/sensors/hals.conf
